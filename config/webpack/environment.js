11ec0f6320244866a3f612800773a2fc30cdd3c0
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-11-18 13:31:16 +0100

    env

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -13,7 +13,8 @@ environment.plugins.prepend(
 environment.plugins.prepend(
   "env",
   new webpack.DefinePlugin({
-    NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    // NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    "process.env": { RAILS_ENV: process.env.RAILS_ENV },
   })
 );
 
