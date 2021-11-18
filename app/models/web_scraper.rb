c24413040aabe72165b9d7dc43af4248331edc93
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-11-19 00:31:24 +0100

    .

--- app/models/web_scraper.rb
+++ app/models/web_scraper.rb
@@ -11,6 +11,7 @@ Webdrivers.logger.level = :DEBUG
 
 Kimurai.configure do |config|
   config.selenium_firefox_path = "/app/vendor/firefox/firefox"
+
 end
 
   # config.selenium_firefox_path = ENV['FIREFOX_BIN'].presence
