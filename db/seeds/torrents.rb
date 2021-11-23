57862779164a295ffc18a198c0b98b2bf44bc3df
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-11-23 12:11:20 +0100

    seeds

--- db/seeds/torrents.rb
+++ db/seeds/torrents.rb
@@ -1,8 +1,7 @@
 Torrent.create(
   title: "Red Notice",
   url: "https://1337x.to/movie/512195/Red-Notice-2021/",
-  genre: "Action,Comedy,Crime,Thriller",
-  release_date: "2021-11-04 00:00:00",
+  release_date: "2021",
   poster_url: "/wdE6ewaKZHr62bLqCn7A2DiGShm.jpg",
   main: true
 )
@@ -10,8 +9,7 @@ Torrent.create(
 Torrent.create(
   title: "Candy Coated Christmas",
   url: "https://1337x.to/movie/866129/Candy-Coated-Christmas-2021/",
-  genre: "Family,Romance",
-  release_date: "2021-11-19 00:00:00",
+  release_date: "2021",
   poster_url: "/HewJEWg0AF9LITCJdEKVJCc4s6.jpg",
   main: true
 )
@@ -19,8 +17,7 @@ Torrent.create(
 Torrent.create(
   title: "Extinct",
   url: "https://1337x.to/movie/664236/Extinct-2021/",
-  genre: "Animation,Comedy,Family,Adventure",
-  release_date: "2021-02-11 00:00:00",
+  release_date: "2021",
   poster_url: "/tRnPT88iD5zgeUPjHqaZznrxk5m.jpg",
   main: true
 )
@@ -28,8 +25,7 @@ Torrent.create(
 Torrent.create(
   title: "Fixed",
   url: "https://1337x.to/movie/894257/Fixed-2021/",
-  genre: "Thriller,Crime",
-  release_date: "2021-11-22 00:00:00",
+  release_date: "2021",
   poster_url: "/d1sCaIV2MIu2QeUZybsGoVRpoQa.jpg",
   main: true
 )
@@ -37,8 +33,42 @@ Torrent.create(
 Torrent.create(
   title: "One December Night",
   url: "https://1337x.to/movie/876444/One-December-Night-2021/",
-  genre: "TV Movie,Comedy,Romance,Family",
-  release_date: "2021-11-13 00:00:00",
+  release_date: "2021",
   poster_url: "/jtzju1ZyZOEC6Vr29zqYaxfJAhr.jpg",
   main: true
 )
+Torrent.create(
+  title: "Dream Horse",
+  release_year: "2021",
+  url: "https://1337x.to/movie/603206/Dream-Horse-2021/",
+  main: true)
+
+Torrent.create(
+  title: "Crimson Peak",
+  release_year: "2015",
+  url: "https://1337x.to/movie/201085/Crimson-Peak-2015/",
+  main: true)
+
+Torrent.create(
+  title: "Malignant",
+  release_year: "2021",
+  url: "https://1337x.to/movie/619778/Malignant-2021/",
+  main: true)
+
+Torrent.create(
+  title: "Slumdog Millionaire",
+  release_year: "2008",
+  url: "https://1337x.to/movie/12405/Slumdog-Millionaire-2...",
+  main: true)
+
+Torrent.create(
+  title: "In The Heights",
+  release_year: "2021",
+  url: "https://1337x.to/movie/467909/In-The-Heights-2021/",
+  main: true)
+
+Torrent.create(
+  title: " 2021.Rock.And.Roll.Hall.Of.Fame.Induction.Ceremon...",
+  release_year: "not yet",
+  url: "https://1337x.to/torrent/5060326/2021-Rock-And-Rol...",
+  main: false)
---
+++ db/seeds/user.rb
@@ -0,0 +1,1 @@
+User.create(username:'Soto', email: 'sotirios.dimitriou@gmail.com', password: 111111, admin: true)
