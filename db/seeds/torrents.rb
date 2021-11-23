eced94cb62d4a511f9fee6b3e1485e158fff10a4
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-11-23 12:40:20 +0100

    without poster

--- db/seeds/torrents.rb
+++ db/seeds/torrents.rb
@@ -2,7 +2,6 @@ Torrent.create(
   title: "Red Notice",
   url: "https://1337x.to/movie/512195/Red-Notice-2021/",
   release_year: "2021",
-  poster_url: "/wdE6ewaKZHr62bLqCn7A2DiGShm.jpg",
   main: true
 )
 
@@ -10,7 +9,6 @@ Torrent.create(
   title: "Candy Coated Christmas",
   url: "https://1337x.to/movie/866129/Candy-Coated-Christmas-2021/",
   release_year: "2021",
-  poster_url: "/HewJEWg0AF9LITCJdEKVJCc4s6.jpg",
   main: true
 )
 
@@ -18,7 +16,6 @@ Torrent.create(
   title: "Extinct",
   url: "https://1337x.to/movie/664236/Extinct-2021/",
   release_year: "2021",
-  poster_url: "/tRnPT88iD5zgeUPjHqaZznrxk5m.jpg",
   main: true
 )
 
@@ -26,7 +23,6 @@ Torrent.create(
   title: "Fixed",
   url: "https://1337x.to/movie/894257/Fixed-2021/",
   release_year: "2021",
-  poster_url: "/d1sCaIV2MIu2QeUZybsGoVRpoQa.jpg",
   main: true
 )
 
@@ -34,7 +30,6 @@ Torrent.create(
   title: "One December Night",
   url: "https://1337x.to/movie/876444/One-December-Night-2021/",
   release_year: "2021",
-  poster_url: "/jtzju1ZyZOEC6Vr29zqYaxfJAhr.jpg",
   main: true
 )
 Torrent.create(
