0ba8e03298eadf441224e666355bc7068e45968a
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-11-23 12:33:09 +0100

    release year

--- spec/system/searches_spec.rb
+++ spec/system/searches_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'test_helper'
 require 'webdrivers'
 
 RSpec.describe "Searches", driver: :selenium_firefox, js: true, type: :system do
@@ -10,24 +11,24 @@ RSpec.describe "Searches", driver: :selenium_firefox, js: true, type: :system do
   end
 
 
-  # it "populate search form" do
-  #   visit home_path
-  #   expect(find('.home_title'))
-  #   fill_in 'search_input', with: "Harry Potter"
-  #   sleep(1)
-  #   expect(find('.search_input'))
-  #   expect(page).to have_css('#results a', :count => 10)
-  #   fill_in 'search_input', with: "Harry Potter and the Philosopher's Stone"
-  #   sleep(1)
-  #   expect(first('#results > a').find('li').find('p')).to have_content("Harry Potter and the Philosopher's Stone")
-  # end
-
-  it "populate movie form" do
-    visit search_path
+  it "populate search form" do
+    visit home_path
+    expect(find('.home_title'))
+    fill_in 'search_input', with: "Harry Potter"
     sleep(1)
-    find('#home_path').click
-    sleep(5)
+    expect(find('.search_input'))
+    expect(page).to have_css('#results a', :count => 10)
+    fill_in 'search_input', with: "Harry Potter and the Philosopher's Stone"
+    sleep(1)
+    expect(first('#results > a').find('li').find('p')).to have_content("Harry Potter and the Philosopher's Stone")
   end
+
+  # it "populate movie form" do
+  #   visit search_path
+  #   sleep(1)
+  #   find('#home_path').click
+  #   sleep(5)
+  # end
 
 end
 
